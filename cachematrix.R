b7f77914fca0dc203cef5114537b1b0a5ca4c39b
ffang <ffang@Gs6102GFEmc206.local>
2014-06-18 08:32:49 -0400

    update by lampmist

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## a matrix constructor function and a function calculates the inverse matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix() is a matrix constructor function.
+## makeCacheMatrix() creates a list containing functions set() and get()
+## a matrix, and set and get the inverse matrix by setInv() and getInv().
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y,nrow,ncol) {
+    x<<-matrix(y,nrow,ncol)
+    inv<<-NULL
+  }
+  get<-function() x
+  setInv<-function(inverse) inv<<-inverse
+  getInv<-function() inv
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() is a function solving for the inverse of a matrix.
+## The object constructed by the makeCacheMatrix constructor, containing
+## a matrix, is passed to cacheSolve, which checks to see if the inverse
+## has already been calculated and return the inverse if so; otherwise
+## it proceeds to calculate the inverse and return.  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getInv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  } else {
+    message("not getting cached data")
+  }
+  data<-x$get()
+  inv<-solve(data)
+  x$setInv(inv)
+  inv
 }
